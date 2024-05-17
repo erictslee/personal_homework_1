d1bd07588026c7e77afc62efbc7fc633cdab8c9e
erictslee <ts5452@naver.com>
2024-05-17 13:18:58 +0900

    ㅇ

---
+++ src/main/java/com/sparta/personahomework/PersonaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.personahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/personahomework/PersonaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.personahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
