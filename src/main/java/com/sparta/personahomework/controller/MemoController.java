095e5be10d56c20124ec5dbc0a94a1c33ded7d39
erictslee <ts5452@naver.com>
2024-05-17 15:29:31 +0900

    memo CRUD

---
+++ src/main/java/com/sparta/personahomework/controller/MemoController.java
@@ -0,0 +1,69 @@
+package com.sparta.personahomework.controller;
+
+import com.sparta.personahomework.dto.MemoRequestDto;
+import com.sparta.personahomework.dto.MemoResponseDto;
+import com.sparta.personahomework.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long ,Memo> memoList=new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo=new Memo(requestDto);
+
+        Long maxId=memoList.size() >0? Collections.max(memoList.keySet())+1:1;
+        memo.setId(maxId);
+
+        memoList.put(memo.getId(), memo);
+
+        MemoResponseDto memoResponseDto=new MemoResponseDto(memo);
+
+        return memoResponseDto;
+    }
+
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos(){
+        List<MemoResponseDto> responseList=memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+
+        if(memoList.containsKey(id)){
+            Memo memo=memoList.get(id);
+
+            memo.update(requestDto);
+            return memo.getId();
+        }else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return id;
+        }else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+
+}
+
+
+
