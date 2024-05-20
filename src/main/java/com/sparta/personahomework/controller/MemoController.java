56ecbb5bdd82e8d0b03e6486a870f2d6d8ab758d
erictslee <ts5452@naver.com>
2024-05-20 14:54:45 +0900

    객체 중복 생성 문제 해결

--- src/main/java/com/sparta/personahomework/controller/MemoController.java
+++ src/main/java/com/sparta/personahomework/controller/MemoController.java
@@ -2,68 +2,40 @@ package com.sparta.personahomework.controller;
 
 import com.sparta.personahomework.dto.MemoRequestDto;
 import com.sparta.personahomework.dto.MemoResponseDto;
-import com.sparta.personahomework.entity.Memo;
+import com.sparta.personahomework.service.MemoService;
+import com.sparta.personahomework.dto.MemoRequestDto;
+import com.sparta.personahomework.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class MemoController {
 
-    private final Map<Long ,Memo> memoList=new HashMap<>();
+    private final MemoService memoService;
+
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.memoService = new MemoService(jdbcTemplate);    }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        Memo memo=new Memo(requestDto);
-
-        Long maxId=memoList.size() >0? Collections.max(memoList.keySet())+1:1;
-        memo.setId(maxId);
-
-        memoList.put(memo.getId(), memo);
-
-        MemoResponseDto memoResponseDto=new MemoResponseDto(memo);
-
-        return memoResponseDto;
+        return memoService.createMemo(requestDto);
     }
 
-
     @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos(){
-        List<MemoResponseDto> responseList=memoList.values().stream()
-                .map(MemoResponseDto::new).toList();
-
-        return responseList;
+    public List<MemoResponseDto> getMemos() {
+        return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-
-        if(memoList.containsKey(id)){
-            Memo memo=memoList.get(id);
-
-            memo.update(requestDto);
-            return memo.getId();
-        }else{
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        if(memoList.containsKey(id)){
-            memoList.remove(id);
-            return id;
-        }else{
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return memoService.deleteMemo(id);
     }
-
-
 }
-
-
-
